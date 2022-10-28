73071aea68d444846c0f751d1f29abe6d633c752
neverFeltAlive <20This.Is.My.Mail00@proronmail.com>
2022-10-29 00:53:35 +0300

    Last Images Added

--- src/components/Contacts/Contacts.tsx
+++ src/components/Contacts/Contacts.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import {IconFeature} from "../About/About";
 import styles from "./Contacts.module.sass";
 import {CONTACTS} from "../../utils/constants";
+import phone from "../../static/images/phone.png";
+import address from "../../static/images/address.png";
+import email from "../../static/images/email.png";
 
 const Contacts = (): JSX.Element => {
     return (
@@ -9,9 +12,9 @@ const Contacts = (): JSX.Element => {
             <h2>Контактная информация</h2>
 
             <div className={styles.featureBar}>
-                <IconFeature text={CONTACTS.tel} src="/phone.png"/>
-                <IconFeature text={CONTACTS.address} src="/address.png"/>
-                <IconFeature text={CONTACTS.email} src="/email.png"/>
+                <IconFeature text={CONTACTS.tel} src={phone}/>
+                <IconFeature text={CONTACTS.address} src={address}/>
+                <IconFeature text={CONTACTS.email} src={email}/>
             </div>
         </section>
     );
